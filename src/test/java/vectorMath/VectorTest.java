fb88127ac076b0ebfc96b22abc062b411a497a3e
Caleb Bissell <calebsbissell@gmail.com>
2017-07-16 20:53:41 -0400

    Tested the three dimensional vector constructor

--- src/test/java/vectorMath/VectorTest.java
+++ src/test/java/vectorMath/VectorTest.java
@@ -6,11 +6,18 @@ import org.junit.Test;
 
 public class VectorTest {
 
-	
 	@Test
-	public void shouldConstructATwoDimensionalVectorCorrectly(){
-		Vector twoDimensionalVector = new Vector(1,2);
+	public void shouldConstructATwoDimensionalVectorCorrectly() {
+		Vector twoDimensionalVector = new Vector(1, 2);
 		assertEquals(1, twoDimensionalVector.getX(), 0.000001);
 		assertEquals(2, twoDimensionalVector.getY(), 0.000001);
+	}
+
+	@Test
+	public void shouldConstructAThreeDimensionalVectorCorrectly() {
+		Vector threeDimensionalVector = new Vector(1, 2, 3);
+		assertEquals(1, threeDimensionalVector.getX(), 0.000001);
+		assertEquals(2, threeDimensionalVector.getY(), 0.000001);
+		assertEquals(3, threeDimensionalVector.getZ(), 0.000001);
 	}
 }
